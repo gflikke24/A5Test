068086abfd9453d9f491340d40b24196bd8522ec
gflikke24 <156116394+gflikke24@users.noreply.github.com>
2024-02-26 10:47:33 -0600

    1st screen done, working on 2nd screen

---
+++ A5Test/Views/FlowerView.swift
@@ -0,0 +1,18 @@
+//
+//  FlowerView.swift
+//  A5Test
+//
+//  Created by Grace Flikke24 on 2/21/24.
+//
+
+import SwiftUI
+
+struct FlowerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlowerView()
+}
